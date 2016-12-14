edd2b5c70f82d49b79208756578bcb53143c729e
Ilya Pronin <mimeticdrift@gmail.com>
2016-12-15 01:15:31 +0300

    +simple upload item

---
+++ upload.js
@@ -0,0 +1,8 @@
+var upload = document.getElementById("upload");
+var loader = document.getElementById("image-file");
+
+upload.addEventListener("click",
+  function(event) {
+    loader.click();
+  }
+);
